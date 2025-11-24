0fa6bfe2774a5b290d5a83a99e77879ccc6558ea
Cervo88 <lorycervino2017@gmail.com>
2025-11-24 13:02:08 +0100

    commit GaraAstronavi 3

--- src/Astronavi.java
+++ src/Astronavi.java
@@ -2,6 +2,44 @@ import java.util.Scanner;
 
 public class Astronavi extends Thread {
     String modello;
-    String nome_Partecipanti;
+    String nome_Partecipante;
     GestoreGara g;
+
+
+    public Astronavi(String modello, String nome_Partecipante) {
+        this.modello = modello;
+        this.nome_Partecipante = nome_Partecipante;
+    }
+
+    public Astronavi(String modello, String nome_Partecipante, GestoreGara g) {
+
+        this.modello = modello;
+        this.nome_Partecipante = nome_Partecipante;
+        this.g = g;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Astronave:" + this.modello);
+
+        gioca();
+        try {
+            sleep(3000);
+        }catch (InterruptedException e){
+            System.err.println("Errore nella transizione");
+        }
+        System.out.println("Gioco Terminato");
+    }
+
+    public  void gioca() {
+        //chiedere un numero in input
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Inserisci la tua posizione: ");
+        // leggere il numero inserito e assegnarla a una variabile
+        int num = sc.nextInt();
+        g.Verifica(num);
+        //mostrare  in input
+        System.out.println("" + modello + "  Sei nella posizione:" + num);
+
+    }
 }
--- src/GestoreGara.java
+++ src/GestoreGara.java
@@ -1,2 +1,16 @@
 public class GestoreGara {
+    int num_Partecipanti;
+
+    public GestoreGara(int num_Partecipanti) {
+        this.num_Partecipanti = num_Partecipanti;
+    }
+
+    public void Verifica(int num) {
+            if (num == num_Partecipanti) {
+            System.out.println(" HAI VINTO");
+        } else {
+            System.out.println(" HAI PERSO");
+        }
+    }
 }
+
